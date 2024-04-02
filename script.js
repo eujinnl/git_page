b2c310ca773c9662a8c65e94ed9b279c945b61dd
eujinnl <eujinnleong@gmail.com>
2024-04-02 22:39:08 +0200

    yes sir

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var timer1 = document.getElementById("timer1");
+var timer2 = document.getElementById("timer2");
+var group1 = Array.from(document.getElementsByClassName("group1"));
+var group2 = Array.from(document.getElementsByClassName("group2"));
+
+var t1 = 3000; // set your timer1 countdown in seconds
+var t2 = 1000; // set your timer2 countdown in seconds
+
+
+
+function toggleVisibility(elements) {
+  elements.forEach(function(element) {
+    element.style.display = element.style.display !== 'none' ? 'none' : 'inline-block';
+  });
+}
+
+function swapTimers() {
+  toggleVisibility(group1);
+  toggleVisibility(group2);
+}
+
+// let's say you want the countdown to end at 12:00 on April 1, 2025 in Bavaria (which is in Central European Time)
+var BachelorDate = moment.tz("2025-04-01 12:00:00", "Europe/Berlin");
+// var MastersDate = moment.tz("2027-04-01 12:00:00", "Europe/Berlin");
+
+setInterval(function() {
+  var now = moment();
+  var remaining = moment.duration(BachelorDate.diff(now));
+  
+  var years = remaining.years();
+  var months = remaining.months();
+  var days = remaining.days();
+  var hours = remaining.hours();
+  var minutes = remaining.minutes();
+  var seconds = remaining.seconds();
+  
+  timer1.innerHTML = years + "y "+ months + "m " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  // timer1.innerHTML = now
+
+  if(yourTargetDate < now) {
+    clearInterval(interval);
+    timer1.innerHTML = "EXPIRED";
+  }
+
+}, 1000);
+
+// setInterval(function() {
+//   timer1.innerHTML = t1 + " seconds remaining";
+//   timer2.innerHTML = t2 + " seconds remaining";
+//   if (t1 > 0) t1--;
+//   if (t2 > 0) t2--;
+// }, 1000);
